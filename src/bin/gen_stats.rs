d9983206151b2f9967f1afd2a5b2b3130f247876
Mubelotix <mubelotix@gmail.com>
2024-09-17 13:01:50 +0200

    Add stats

--- src/bin/gen_stats.rs
+++ src/bin/gen_stats.rs
@@ -16,6 +16,8 @@ fn main() {
     let mut board = vec![vec![""; 250]; 250];
     let mut board_usernames = vec![vec![""; 250]; 250];
     let mut recovered_counts = vec![vec![0; 250]; 250];
+    let mut virgin_counts_by_user = HashMap::new();
+    let mut non_virgin_counts_by_user = HashMap::new();
     for line in data.lines() {
         let mut parts = line.split("\t");
         let Some(time) = parts.next() else {continue};
@@ -33,6 +35,13 @@ fn main() {
             eprintln!("Unknown user: {}", username);
             continue;
         };
+
+        let virgin_pixel = board_usernames[x as usize][y as usize].is_empty();
+        if virgin_pixel {
+            *virgin_counts_by_user.entry(username).or_insert(0) += 1;
+        } else {
+            *non_virgin_counts_by_user.entry(username).or_insert(0) += 1;
+        }
 
         board[x as usize][y as usize] = faction;
         board_usernames[x as usize][y as usize] = username;
@@ -57,6 +66,8 @@ fn main() {
             *count += 1;
         }
     }
+
+    let very_active_users = user_counts.iter().filter(|(_, &c)| c >= 25).map(|(u, _)| **u).collect::<HashSet<_>>();
 
     println!("\nPixel counts:");
     for (faction, count) in stats.iter() {
@@ -142,7 +153,36 @@ fn main() {
     println!("\nParticipation rates:");
     for faction in ["MECA", "MRIE", "CFI", "EP", "GM", "ITI", "LH"] {
         let count = USERS.iter().filter(|(_, f)| **f == faction).count();
-        let participating = user_counts.iter().filter(|(&&u, &c)| c >= 10 && *USERS.get(u).unwrap() == faction).count();
+        let participating = very_active_users.iter().filter(|u| *USERS.get(*u).unwrap_or(&"") == faction).count();
         println!("{}: {:.02}%", faction, participating as f64 / count as f64 * 100.0);
+    }
+
+    println!("\nConflict rates by faction:");
+    for faction in ["MECA", "MRIE", "CFI", "EP", "GM", "ITI", "LH"] {
+        let participating_users = USERS.iter().filter(|(_, f)| **f == faction).map(|(u, _)| *u).collect::<HashSet<_>>();
+        let mut virgin_count = 0;
+        let mut non_virgin_count = 0;
+        for user in participating_users {
+            virgin_count += virgin_counts_by_user.get(&user).unwrap_or(&0);
+            non_virgin_count += non_virgin_counts_by_user.get(&user).unwrap_or(&0);
+        }
+        println!("{}: {:.02}% ", faction, non_virgin_count as f64 / (virgin_count + non_virgin_count) as f64 * 100.0);
+    }
+
+    println!("\nMost conflictual users:");
+    let mut conflict_rates = Vec::new();
+    for user in very_active_users.iter() {
+        let virgin_count = *virgin_counts_by_user.get(user).unwrap_or(&0);
+        let non_virgin_count = *non_virgin_counts_by_user.get(user).unwrap_or(&0);
+        conflict_rates.push((user, non_virgin_count as f64 / (virgin_count + non_virgin_count) as f64 * 100.0));
+    }
+    conflict_rates.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    for (user, rate) in conflict_rates.iter().take(10) {
+        println!("{}: {:.02}%", user, rate);
+    }
+
+    println!("\nLeast conflictual users:");
+    for (user, rate) in conflict_rates.iter().rev().take(10) {
+        println!("{}: {:.02}%", user, rate);
     }
 }
